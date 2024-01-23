545431eb54a4cd0d05d695cbf84a0774ceaba34e
Lucas <lucas.eartsup@gmail.com>
2024-01-23 20:41:45 +0100

    particleEnemy

--- Assets/Scripts/Bullet/ImpactSomething.cs
+++ Assets/Scripts/Bullet/ImpactSomething.cs
@@ -2,20 +2,31 @@ using UnityEngine;
 
 public class ImpactSomething : MonoBehaviour
 {
-  [SerializeField] private Score _score;
+    [SerializeField] private Score _score;
 
-  private void Awake()
-  {
-    _score = FindObjectOfType<Score>();
-  }
+    [SerializeField] private ParticleSystem _bloodParticles;
+    private ParticleSystem _bloodParticlesInstance;
 
-  private void OnTriggerEnter2D(Collider2D other)
-  {
-    if (other.gameObject.CompareTag("IA"))
+    private void Awake()
     {
-      _score.scoreCounter += 10;
-      Destroy(other.gameObject);
-      Destroy(gameObject);
+        _score = FindObjectOfType<Score>();
     }
-  }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("IA"))
+            {
+                _score.scoreCounter += 10;
+
+                SpawnParticles();
+
+                Destroy(other.gameObject);
+                Destroy(gameObject);
+            }
+    }
+
+    private void SpawnParticles()
+    {
+        _bloodParticlesInstance = Instantiate(_bloodParticles, transform.position, Quaternion.identity);
+    }
 }
