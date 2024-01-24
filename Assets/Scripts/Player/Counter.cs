2b9a30ebd582d91b3516b45cf8004efab6e5e414
Ickise <enzo.dufour05@gmail.com>
2024-01-24 02:35:05 +0100

    Counter

---
+++ Assets/Scripts/Player/Counter.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    [Header("À set up")] 
+    [SerializeField] private float counterCooldown = 2f, timeToDestroyEnemy = 4f, capsuleAngle = 0f, distanceToDetectEnemy = 0f;
+
+    [SerializeField] private Transform capsuleCastTransform;
+
+    [SerializeField] private Vector2 capsuleSize = new Vector2(1f, 0.1f);
+    [SerializeField] private Vector2 force = new Vector2(500, 500);
+
+    [SerializeField] private LayerMask layerEnemy;
+
+    private float timeToCounter;
+
+    private IA _ia;
+
+    private Rigidbody2D enemyRigidbody2D;
+
+    private Collider2D enemyCollider2D;
+
+    private RaycastHit2D capsuleCastHitEnemy;
+
+    private void Start()
+    {
+        InputReader._instance.onCounterEvent.AddListener(GetIAScript);
+    }
+
+    private void Update()
+    {
+        timeToCounter += Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        BumpEnemy();
+    }
+
+    private void GetIAScript()
+    {
+        if (timeToCounter >= counterCooldown)
+        {
+            timeToCounter = 0;
+
+            capsuleCastHitEnemy = Physics2D.CapsuleCast(capsuleCastTransform.position, capsuleSize,
+                CapsuleDirection2D.Vertical,
+                capsuleAngle, Vector2.right, distanceToDetectEnemy, layerEnemy);
+
+            if (capsuleCastHitEnemy.collider != null)
+            {
+                _ia = capsuleCastHitEnemy.collider.gameObject.GetComponent<IA>();
+            }
+        }
+    }
+
+    private void BumpEnemy()
+    {
+        if (_ia != null)
+        {
+            enemyRigidbody2D = _ia.GetComponent<Rigidbody2D>();
+            enemyCollider2D = _ia.GetComponent<Collider2D>();
+
+            Destroy(_ia);
+            enemyCollider2D.isTrigger = true;
+            enemyRigidbody2D.velocity = transform.right * force;
+
+            Destroy(_ia.gameObject, timeToDestroyEnemy);
+        }
+    }
+}
