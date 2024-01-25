2d9c61798d08031e97a2a0cf39ebda1bd6493cab
Ickise <enzo.dufour05@gmail.com>
2024-01-26 00:08:23 +0100

    Menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,6 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer spriteRendererMenu;
+
+    [SerializeField] private Sprite loreSprite;
+
+    [SerializeField] private GameObject continueToGame;
+
+    [SerializeField] private GameObject[] listToDisable;
+
+    public void LoreExplanation()
+    {
+        spriteRendererMenu.sprite = loreSprite;
+        continueToGame.SetActive(true);
+        
+        foreach (var gameObject in listToDisable)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
     public void Play()
     {
         SceneManager.LoadScene("MAP");
@@ -10,16 +29,15 @@ public class Menu : MonoBehaviour
 
     public void Credit()
     {
-
     }
 
     public void Exit()
     {
-        #if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;
-        #else
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
 				        Application.Quit();
-        #endif
+#endif
     }
 
     public void GoToMenu()
